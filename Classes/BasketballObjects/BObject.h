67f6bc5415509404c10f8d13086bd8d4c8991bad
kalter704 <lantratov.vasia@yandex.ru>
2015-05-03 15:41:43 +0300

    Revert "Revert "Adding basketball objects""
    This reverts commit aca75adbec95bdfb626fb683e252d0bf0db6480f.

---
+++ Classes/BasketballObjects/BObject.h
@@ -0,0 +1,25 @@
+#ifndef __BASKETBALL_OBJECT_H__
+#define __BASKETBALL_OBJECT_H__
+
+
+class MyClass
+{
+public:
+	MyClass();
+	~MyClass();
+
+private:
+
+};
+
+MyClass::MyClass()
+{
+}
+
+MyClass::~MyClass()
+{
+}
+
+};
+
+#endif
