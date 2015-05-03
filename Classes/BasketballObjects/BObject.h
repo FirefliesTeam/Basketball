ec587330872d874dfb66bb02055585fa4c7c029b
Павел Пивкин <Pivkin.Pavel.11@yandex.ru>
2015-05-03 16:08:51 +0300

    Added absrtract class
    Added absrtract class

--- Classes/BasketballObjects/BObject.h
+++ Classes/BasketballObjects/BObject.h
@@ -5,23 +5,29 @@
 
 class BObject
 {
-public:
-	BObject();
-	~BObject();
+	public:
+		BObject();
+		BObject();
+		~BObject();
+		virtual void setPosition() = 0;
 
-protected:
-	cocos2d::Sprite sprite
+	protected:
+		cocos2d::Sprite *sprite;
+		cocos2d::PhysicsBody *physics_size;
+		cocos2d::Size visible_size;
+		cocos2d::Vec2 origin;
 
-};
+	};
 
-BObject::BObject()
-{
-}
+	BObject::BObject(){
 
-BObject::~BObject()
-{
-}
+		visible_size = cocos2d::Director::getInstance() -> getVisibleSize();
+		origin = cocos2d::Director::getInstance() -> getVisibleOrigin();
+	}
 
+	BObject::~BObject() {
+
+	}
 };
 
 #endif
