3a724f2cf993a1da1fc8360e85f267644cd40374
Павел Пивкин <Pivkin.Pavel.11@yandex.ru>
2015-06-05 10:52:48 +0300

    Правка и добавления в DefForPlanks
    Поправлены ошибки в написании слов - характеристик материала.
    Добавлены стоимости планок для использования их в конструкторе BPlank

--- Classes/BasketballObjects/BOMetalPlank.cpp
+++ Classes/BasketballObjects/BOMetalPlank.cpp
@@ -6,7 +6,7 @@ USING_NS_CC;
 BMetalPlank::BMetalPlank(int cost, int priority, float scale, cocos2d::Layer *layer): BPlank(cost, scale) {
     sprite = Sprite::create(METAL_PLANK);
     sprite->setScaleX(scale);
-    material = PhysicsMaterial(METAL_PLANK_DENSITY, METAL_PLANK_RESTITUTION, METAL_PALNK_FRICTION);
+    material = PhysicsMaterial(METAL_PLANK_DENSITY, METAL_PLANK_RESTITUTION, METAL_PLANK_FRICTION);
 
     body = PhysicsBody::createBox(Size(sprite->getContentSize().width * scale, sprite->getContentSize().height), material);
     body->setDynamic(false);
--- Classes/BasketballObjects/BORubberPlank.cpp
+++ Classes/BasketballObjects/BORubberPlank.cpp
@@ -6,7 +6,7 @@ USING_NS_CC;
 BRubberPlank::BRubberPlank(int cost, int priority, float scale, cocos2d::Layer *layer): BPlank(cost, scale) {
     sprite = Sprite::create(RUBBER_PLANK);
     sprite->setScaleX(scale);
-    material = PhysicsMaterial(RUBBER_PLANK_DENSITY, RUBBER_PLANK_RESTIITUTION, RUBBER_PLANK_FRICTION);
+    material = PhysicsMaterial(RUBBER_PLANK_DENSITY, RUBBER_PLANK_RESTITUTION, RUBBER_PLANK_FRICTION);
 
     body = PhysicsBody::createBox(Size(sprite->getContentSize().width * scale, sprite->getContentSize().height), material);
     body->setDynamic(false);
